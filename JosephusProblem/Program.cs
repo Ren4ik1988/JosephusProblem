d7ec961c7cd17a9e146305bedd8da971f2d121bb
Ren4ik <ren.zigan@gmail.com>
2018-01-22 02:40:17 +0300

    Коммит

---
+++ JosephusProblem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JosephusProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class DoWork
+    {
+        int input;
+        int step;
+        List<int> spisok;
+
+        public void Do()
+        {
+            int.TryParse(Console.ReadLine().Split(' ')[0], out input);
+            int.TryParse(Console.ReadLine().Split(' ')[1], out step);
+            spisok = new List<int>(input);
+        }
+
+        void stepByStep()
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                spisok.Add(i);
+            }
+
+            
+        }
+    }
+}
